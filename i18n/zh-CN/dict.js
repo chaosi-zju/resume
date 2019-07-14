4ed7636dcc5a2d1c51b6963c689d73d5adfcc640
chaosi-zju <chaosi@zju.edu.cn>
2019-07-14 14:01:46 +0800

    init

---
+++ i18n/zh-CN/dict.js
@@ -0,0 +1,50 @@
+module.exports = {
+    experience_type: {
+        "internship": "（实习）"
+    },
+    skill_type: {
+        "about": "关于我",
+        "language": "语言与技能",
+        "contest": "赛事奖励",
+        "miscellaneous" : "其他"
+    },
+    education_type: {
+        "undergraduate": "（本科）",
+        "graduate":" (硕士)"
+    },
+    link_type: {
+        "zhihu": "知乎",
+        "doubanbook": "豆瓣读书",
+        "repo": "源代码",
+        "pdf": "PDF 下载"
+    },
+    section_names: {
+        "education": {
+            "ch": "教育经历",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "语言水平",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "项目与工作经验",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "个人概况",
+            "en": "Skills"
+        }
+    },
+    lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~",
+    dict: {
+        source_code: "源代码",
+        demo: "Demo"
+    },
+    project_type: {
+    	"outer": "个人外包项目",
+    	"labor": "实验室科研项目",
+    	"school": "国家级大学生创新创业项目",
+        "personal": "个人项目"
+    }
+};
